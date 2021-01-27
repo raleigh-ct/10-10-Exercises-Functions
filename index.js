49af228cda38b41ab478f1a91b86d8e6e5164f42
Raleigh Moore <raleighcmoore@gmail.com>
2021-01-27 21:51:54 +0000

    Added More Exercise Answers

--- index.js
+++ index.js
@@ -86,14 +86,71 @@ let dStair = '';
 return dStair;
 }
 
-console.log(makeDownwardStairs(5));
+// console.log(makeDownwardStairs(5)); // Worked as Expected
 
 // dStair += makeLine(height-m) + '\n';
 
 // 2. Space Line Function
 
 function makeSpaceLine(numSpaces, numChars) {
+  let spaces = '';
+  let chars = '';
   let spaceLine = '';
 
-  for ()
+  for (i=0; i<numSpaces; i++) {
+    spaces += ' ';
+  }
+  for (j=0; j < numChars; j++) {
+    chars += '#';
+  }
+
+  spaceLine = spaces+chars+spaces;
+
+  return spaceLine;
 }
+
+// console.log(makeSpaceLine(2,6)); // Worked as Expected
+
+
+
+// 3. Triangle Function 
+
+function makeIsoscelesTriangle(height) {
+
+let triRow = '';
+
+  for (k=0; k < height; k++) {
+    
+    if (k < height -1) {
+      triRow += makeSpaceLine((height - k - 1), (2 * k + 1)) + '\n';
+  } else {
+    triRow += makeSpaceLine((height - k - 1), (2 * k + 1));
+  }
+
+  return triRow;
+}
+}
+
+// console.log(makeIsoscelesTriangle(7)); // Worked as expected
+
+
+// 10.10.3
+// 1. Diamond
+
+
+
+
+function makeInvertedIsoscelesTriangle(height) {
+
+let invTriRow = '';
+
+  for (q=0; q < height; q++) {
+    invTriRow += makeSpaceLine((2 / q + 1), (height - q)) + '\n';
+  }
+
+  return invTriRow;
+
+}
+
+
+console.log(makeInvertedIsoscelesTriangle(5));
